a122a66211246479c4649ac5c97929a0f4c25cb7
Shoili Chowdhury <43523898+Shoili06@users.noreply.github.com>
2019-05-15 18:25:53 +0530

    Assignment2 done

---
+++ src/main.kt
@@ -0,0 +1,22 @@
+import java.util.*
+
+fun main(){
+    val input = Scanner(System.`in`)
+    println("Enter first number : ")
+    val a= input.nextInt()
+    println("Enter second number : ")
+    val b= input.nextInt()
+    println("Enter choice(+,-,*,/,%) : ")
+    val choice:String= input.next()
+    val result = {a:Int,b:Int,choice:String ->
+        when(choice){
+            "+" -> a+b
+            "-" -> a-b
+            "*" -> a*b
+            "/" -> a/b
+            "%" -> a%b
+            else -> 0
+        }
+    }
+    println("$a $choice $b = "+result(a,b,choice))
+}
